e3fe3bb78f5e495ddd9204aeeb17ec4bcc1abe4a
satya <satyasairam144@gmail.com>
2021-11-17 14:39:56 +0530

    terraform for ansible

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region               = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket             = "my-ansible-terraform-bucket"
-    key                = "ansible/terraform.tfstate"
-    region             = "us-east-1"
-    dynamodb_table     = "new_dynamodb_table"
-  }
-}
+//terraform {
+  //backend "s3" {
+    //bucket             = "my-ansible-terraform-bucket"
+    //key                = "ansible/terraform.tfstate"
+    //region             = "us-east-1"
+    //dynamodb_table     = "new_dynamodb_table"
+  //}
+//}
