c0dc1ecebafa8ef266d0984c0a311a217bec5610
satya <satyasairam144@gmail.com>
2021-11-17 14:19:16 +0530

    terraform for ansible

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket             = "new-bucket-ansible-terraform"
-    key                = "ansible-terraform/terraform_remote-state_file"
+    key                = "ansible/terraform_remote-state_file"
     region             = "us-east-1"
     dynamodb_table     = "new_dynamodb_table"
   }
