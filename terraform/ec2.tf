47a28015a4975a66d7f73c90a0aabaae8500a5c2
satya <satyasairam144@gmail.com>
2021-11-18 12:03:13 +0530

    one component frontend

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,8 +9,9 @@ resource "aws_instance" "instances" {
 }
 
 resource "local_file" "inventory-file" {
-  content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[0]}\n[PAYMENT]\n${aws_instance.instances.*.private_ip[5]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[4]}\n[USER]\n${aws_instance.instances.*.private_ip[2]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[1]}\n[CART]\n${aws_instance.instances.*.private_ip[3]}\n[REDIS]\n${aws_instance.instances.*.private_ip[8]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[9]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[6]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[7]}\n"
-  filename    = "/tmp/inv-roboshop"
+  content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[0]}"
+#  \n[PAYMENT]\n${aws_instance.instances.*.private_ip[5]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[4]}\n[USER]\n${aws_instance.instances.*.private_ip[2]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[1]}\n[CART]\n${aws_instance.instances.*.private_ip[3]}\n[REDIS]\n${aws_instance.instances.*.private_ip[8]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[9]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[6]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[7]}\n"
+  filename    = "/tmp/inv-roboshop1"
 }
 
 resource "aws_route53_record" "records" {
